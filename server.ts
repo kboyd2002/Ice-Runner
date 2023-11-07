4e665244f92d2fc1af7779890537ecec287026b9
Sylvestor <145170477+cbbartlett@users.noreply.github.com>
2023-11-07 11:25:24 -0600

    Added login and API routes.

--- server.ts
+++ server.ts
@@ -1,25 +1,39 @@
-import express, {Request, Response } from "express";
+import express from "express";
+import path from "path";
+import { Request, Response } from "express";
+import apiRoutes from './controllers/api/';
 import session from "express-session";
-import exphes from "express-handlebars";
+import { engine } from "express-handlebars";
 import dotenv from "dotenv";
 import { Model, InferAttributes , InferCreationAttributes, CreationOptional } from 'sequelize';
+import loginRouter from "./ts/login";
+import logoutRouter from "./ts/logout"
 
 const app = express();
-const port = 3001;
-
-// Setup Middleware
 
 app.get("/", (req: Request, res: Response) => {
-    return res.json();
+    return res.render('home');
 });
 
-app.post()
+// Handlebars routes
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Link the public folder
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+// Register API routes
+api.use('/api', apiRoutes);
+
+// Login and logout routes
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
 
 // Listen to server
-try {
-    app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`)
-    })
-} catch (error) {
-    console.log(`Error occurred: ${error.message}`)
-}
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
