a22063eb740c6589604d3cbff9397efac3c9d38b
Sylvestor <145170477+cbbartlett@users.noreply.github.com>
2023-11-08 10:22:08 -0600

    Fix middleware logic.

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,10 +1,31 @@
-import { Router } from 'express';
-import apiRoutes = require('./api');
-import playRoutes = require('./play-routes');
+import { Router, Request, Response, NextFunction } from 'express';
+import { default as apiRoutes } from './api';
+import { default as playRoutes } from './play-routes';
 
 const router: Router = Router();
 
-router.use('/', playRoutes);
-router.use('/api', apiRoutes);
+// Setup Middleware
+router.use('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log('Request:', req.method, req.url);
+  next();
+});
+
+// Logging
+router.use('/api', (req: Request, res: Response, next: NextFunction) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  next();
+});
+
+// Error Handling
+router.use('/', (req: Request, res: Response, next: NextFunction) => {
+  try {
+    next();
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 export default router;
